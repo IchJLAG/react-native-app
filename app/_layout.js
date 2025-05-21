be3ea124da8f3a2a20bde746c4c15ac932e06e9e
Jose Luis Alvarez <jose_luis_alvarez_g@hotmail.com>
2025-05-20 21:48:36 -0500

    solo es una prueba

---
+++ app/_layout.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Layout() {
+  return <View ClassName="items-center justify-center flex-1 bg-black">
+    <Text>Hola text tesxto esotoskdkdkskd prueba prueba</Text>
+     </View>;
+ 
+  
+}
---
+++ app/index.js
@@ -0,0 +1,8 @@
+import Logo from '../components/Logo.jsx';
+
+
+export default function miApp() {
+ 
+    return <Logo/>;
+    
+}
