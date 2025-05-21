f23d2ca55676ff6cfd6f743c2bfa2b597bab12a1
Jose Luis Alvarez <jose_luis_alvarez_g@hotmail.com>
2025-05-20 22:11:17 -0500

    adadadad

--- app/_layout.js
+++ app/_layout.js
@@ -1,9 +1,10 @@
+import { Slot } from "expo-router";
 import { View, Text } from "react-native";
 
 export default function Layout() {
-  return <View ClassName="items-center justify-center flex-1 bg-black">
-    <Text>Hola text tesxto esotoskdkdkskd prueba prueba</Text>
-     </View>;
- 
-  
+  return (
+    <View>
+      <Slot />
+    </View>
+  );
 }
--- components/Logo.jsx
+++ components/Logo.jsx
@@ -4,7 +4,9 @@ import React from 'react'
 export default function Logo() {
   return (
     <View>
-      <Text>Logo</Text>
+      <Text className="text-red-400"> Lerum Ipsum </Text>    
+      <Text> Lorem ipsum dolor sit amet, consectetur adipiscing elit. </Text>
+      <Text className="text-red-950"> Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </Text>
     </View>
   )
 }
